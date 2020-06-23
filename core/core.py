b9c0d4977795fb4722c4971a2af57e7498a60725
Michael Berry <trismegustis@gmail.com>
2020-06-23 14:36:43 -0400

    All important todo* files move to a central directory.

--- core/core.py
+++ core/core.py
@@ -9,6 +9,16 @@ import os
 __version__ = "0.1.0"
 options = {}
 db = None
-ini_fn = os.path.join(os.getenv("HOME"), ".todo.ini")
-lists_fn = os.path.join(os.getenv("HOME"), ".todo_lists.json")
-log_fn = os.path.join(os.getenv("HOME"), ".todo.log")
+todo_dir = os.path.join(os.getenv("HOME"), ".todo")
+
+if not os.path.exists(todo_dir):
+    try:
+        os.mkdir(todo_dir)
+    except OSError as e:
+        print(f"Error: {e}")
+        exit(1)
+
+
+ini_fn = os.path.join(todo_dir, "todo.ini")
+lists_fn = os.path.join(todo_dir, "todo_lists.json")
+log_fn = os.path.join(todo_dir, "todo.log")
