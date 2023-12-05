e631f4555ba5df56a31ddc977c945f95320d057f
Michael Berry <trismegustis@gmail.com>
2023-12-05 10:15:44 -0500

    Revert "Modernized the way the main window is centered."
    This reverts commit 4b2addb992304fc34eb55cbffddbdb5d673144d2.

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -7,7 +7,6 @@ import os
 
 from PyQt5 import QtGui, QtWidgets, QtPrintSupport
 from PyQt5.QtCore import QPersistentModelIndex
-from PyQt5.QtWidgets import QDesktopWidget
 
 from core import defaults, json_helpers
 from core.TodoDatabase import todo_index, sort_active_list, TodoList
@@ -640,10 +639,11 @@ class CreateMainWindow(QtWidgets.QMainWindow):
 
     def center(self):
         """Place the main window in the center of the screen."""
-        qt_rectangle = self.frameGeometry()
-        center_point = QDesktopWidget().availableGeometry().center()
-        qt_rectangle.moveCenter(center_point)
-        self.move(qt_rectangle.topLeft())
+        screen = QtWidgets.QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move(
+            (screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2
+        )
 
     def set_progress_bar_max(self, maximum):
         self.progressBar.setMaximum(maximum)
